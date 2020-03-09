1cfb6870ceac5c9c80fb359cd4419fd28f9710e5
ignisii <ignisii@outlook.com>
2020-03-09 16:34:43 +0000

    Updates

---
+++ precache-manifest.d584f2bbe691df1456a2c1a17efc8467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8b1caafa92e256d3c31b44f0f7662a",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "d1e479691dd2ea5c1e55",
+    "url": "/swapi/static/css/main.c58d5c5e.chunk.css"
+  },
+  {
+    "revision": "e1e56902d346008d7995",
+    "url": "/swapi/static/js/2.04b28158.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/swapi/static/js/2.04b28158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e479691dd2ea5c1e55",
+    "url": "/swapi/static/js/main.3ed8172e.chunk.js"
+  },
+  {
+    "revision": "7a0a9aea52c3c364cce6",
+    "url": "/swapi/static/js/runtime-main.cf6bee74.js"
+  }
+]);
